1f47a762ec940a25447cb50dbfb7459b432bd266
Titouan Vial <titouan.vial@grenoble-inp.org>
2026-06-25 14:54:11 +0200

    feat: Add mbake formatter for Makefiles

--- init.lua
+++ init.lua
@@ -816,6 +816,7 @@ require('lazy').setup({
         javascript = { 'prettierd', 'biome', stop_after_first = true },
         vhdl = { 'vsg' },
         c = { 'clang-format' },
+        make = { 'bake' },
         -- Conform can also run multiple formatters sequentially
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
